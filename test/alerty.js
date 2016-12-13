8efc641df88a2b7481754827001250c5c9d57651
Kyle Lin <kylejlin.us@gmail.com>
2016-12-12 22:30:45 -0800

    Create alerty.js

---
+++ test/alerty.js
@@ -0,0 +1,9 @@
+var myFoo = new Foo('Yo');
+alert(myFoo.name);
+alert('for var i in this \' hi \'');
+var True = true;
+var False = false;
+var Undefined = undefined;
+var Nil = Null;
+var Args = arguments;
+var This = this;
